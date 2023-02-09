382e4486973d3abff15e057b23ccb055429fd8b8
Deepak B H <94977558+iamdeepakbh@users.noreply.github.com>
2023-02-09 12:33:36 +0530

    Add files via upload

---
+++ email sender/mailsend.py
@@ -0,0 +1,19 @@
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path
+
+html = Template(Path('index.html').read_text())
+email = EmailMessage()
+email['from'] = 'Sender'
+email['to'] = 'Receiver'
+email['subject'] = 'Text email'
+
+email.set_content(html.substitute({'name': 'Ram'}), 'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+  smtp.ehlo()
+  smtp.starttls()
+  smtp.login('<your email address>', '<your password>')
+  smtp.send_message(email)
+  print('all good')
